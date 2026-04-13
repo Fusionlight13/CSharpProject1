1867300a53e32c5167f2a08d1054cb9dab4e679c
Tony <tonyl10000w@gmail.com>
2026-04-12 22:03:16 -0500

    Added Waypoint class

---
+++ gitPractice1/Waypoint.cs
@@ -0,0 +1,10 @@
+﻿namespace gitPractice1;
+
+public class Waypoint(string name, string description, Position position)
+{
+    
+    public string Name { get; } = name;
+    public string Description { get; } = description;
+    public Position Position { get; } = position;
+    
+}
