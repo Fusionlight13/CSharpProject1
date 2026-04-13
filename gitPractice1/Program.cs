bf1cd45a6534f8b7a5d3b47cdc01b9b6eeba06cf
Tony <tonyl10000w@gmail.com>
2026-04-12 22:47:58 -0500

    Added a general manager class to store the full objects. Also added a way to add and remove an object.

--- gitPractice1/Program.cs
+++ gitPractice1/Program.cs
@@ -2,8 +2,14 @@
 
 var wp = new Waypoint("Nether Hub", "My home base", new Position(x:34, y: 100, z: 45));
 
+var mgr = new WayPointManager();
+
+
+mgr.Add(wp);
+
 Console.WriteLine(wp.Name);
 Console.WriteLine(wp.Description);
 Console.WriteLine(wp.Position.X);
 Console.WriteLine(wp.Position.Y);
 Console.WriteLine(wp.Position.Z);
+
---
+++ gitPractice1/WayPointManager.cs
@@ -0,0 +1,23 @@
+﻿namespace gitPractice1;
+
+public class WayPointManager
+{
+    
+    private readonly List<Waypoint> _waypoints = [];
+    
+    public IReadOnlyList<Waypoint> Waypoints => _waypoints;
+    
+
+    public void Add(Waypoint ws)
+    {
+        _waypoints.Add(ws);
+    }
+    
+    public void Remove(Waypoint ws)
+    {
+        _waypoints.Remove(ws);
+    }
+    
+    
+    
+}
