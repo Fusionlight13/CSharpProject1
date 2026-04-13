186576a7e948b5aaa614c908a1fad7308ae57e52
Tony <tonyl10000w@gmail.com>
2026-04-12 21:58:56 -0500

    Commit test

--- gitPractice1/Position.cs
+++ gitPractice1/Position.cs
@@ -1,6 +1,12 @@
 ﻿namespace gitPractice1;
 
-public class Position
+public readonly struct Position(double x, double y, double z)
 {
-    
+    public double X { get; } = x;
+    public double Y { get; } = y;
+    public double Z { get; } = z;
+
+    public Position(int x, int z) : this(x, y: 0, z: z)
+    {
+    }
 }
