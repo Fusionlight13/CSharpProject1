4979773cbc5ffb9e92f758eb9943d4e93bfcc2f8
Tony <tonyl10000w@gmail.com>
2026-04-12 22:31:17 -0500

    Added demo to Program.cs

--- gitPractice1/Position.cs
+++ gitPractice1/Position.cs
@@ -6,7 +6,7 @@ public readonly struct Position(double x, double y, double z)
     public double Y { get; } = y;
     public double Z { get; } = z;
 
-    public Position(int x, int z) : this(x, y: 0, z: z)
+    public Position(int x, int z) : this(x:x, y: 0, z: z)
     {
     }
 }
--- gitPractice1/Program.cs
+++ gitPractice1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using gitPractice1;
+
+var wp = new Waypoint("Nether Hub", "My home base", new Position(x:34, y: 100, z: 45));
+
+Console.WriteLine(wp.Name);
+Console.WriteLine(wp.Description);
+Console.WriteLine(wp.Position.X);
+Console.WriteLine(wp.Position.Y);
+Console.WriteLine(wp.Position.Z);
